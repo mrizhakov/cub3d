9c56c03da161ffec114f7b32148727c1070a3b3c
Michael Rizhakov <m.rizhakov@gmail.com>
2024-05-11 20:07:10 +0200

    ?

--- src/raycast.c
+++ src/raycast.c
@@ -6,7 +6,7 @@
 /*   By: mrizakov <mrizakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 16:58:52 by mrizakov          #+#    #+#             */
-/*   Updated: 2024/05/11 19:23:14 by mrizakov         ###   ########.fr       */
+/*   Updated: 2024/05/11 20:06:26 by mrizakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -224,18 +224,13 @@ void    draw_3d_projection(t_game *game_data, int column_id, t_raycast *ray)
             (uint32_t)column_id, (uint32_t)wall_bottom_pixel, 
             game_data->player->color);
     //draw floor
-    
-    
     drawLine((uint32_t)column_id, (uint32_t)wall_bottom_pixel, 
             (uint32_t)column_id, (uint32_t)WINDOW_HEIGHT-1, 
             game_data->floor.rgb_color);
     //draw celing
     drawLine((uint32_t)column_id, (uint32_t)wall_top_pixel, 
             (uint32_t)column_id, (uint32_t)0, 
-            game_data->ceiling.rgb_color);
-
-    
-            
+            game_data->ceiling.rgb_color);        
 }
 
 
